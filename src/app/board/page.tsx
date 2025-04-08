58c2c2cece1a0be50e05396e4a0117f3d7cc1c08
Damian <damiancrene@gmail.com>
2025-04-08 13:50:19 -0400

    new cards

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -15,7 +15,7 @@ import { createBoard, createDoc } from "@/app/actions/actions";
 import useSWR from "swr";
 import { Board } from "@prisma/client";
 import AddIcon from "@mui/icons-material/Add";
-
+import NotesIcon from "@mui/icons-material/Notes";
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 111, 12, 13, 14, 15, 16];
 const fetcher = (url: any) => fetch(url).then((r) => r.json());
 
@@ -35,42 +35,52 @@ export default function IndexPage() {
   return (
     <>
       <div className="w-full h-[87vh] overflow-y-scroll  grid grid-cols-1 grid-rows-3 lg:pt-2 lg:pl-2 ">
-        <div className="w-full bg-gray-600 row-span-1 p-5 grid grid-cols-6 gap-5">
+        <div className="w-full  row-span-1 p-5 grid grid-cols-6 gap-5">
           <Card
+            isPressable
+            isFooterBlurred
             className={
-              "w-36 rounded-sm grid grid-rows-3 border-4 border-dashed bg-transparent border-gray-600 hover:scale-105 hover:z-50"
+              "dark:bg-slate-800 w-36 border-dashed bg-transparent border-gray-600 hover:scale-105 hover:z-50 h-full"
             }
           >
             <CardBody
               onClick={() => newBoard()}
-              className="text-5xl text-center mt-auto row-start-2"
+              className="justify-items-center w-full text-6xl "
             >
-              <AddIcon fontSize="large" className=""></AddIcon>
+              <AddIcon
+                fontSize="inherit"
+                className="ml-auto mr-auto mt-auto mb-auto "
+              ></AddIcon>
             </CardBody>
-
-            <CardFooter className="text-small text-center justify-items-center">
-              New Board
-            </CardFooter>
+            <CardFooter className="text-sm">New Document</CardFooter>
           </Card>
+
           <Card
             isPressable
-            onPress={() => console.log("HELLOO")}
+            isFooterBlurred
             className={
-              " w-36 rounded-sm grid grid-rows-3 bg-transparent border-4 border-dashed border-gray-600 hover:scale-105 hover:z-50"
+              "dark:bg-slate-800 w-36 border-dashed bg-transparent border-gray-600 hover:scale-105 hover:z-50 h-full"
             }
           >
-            <CardBody className="text-center mt-auto row-start-1 font-semibold text-xl">
-              Template 1
+            <CardBody
+              onClick={() => newBoard()}
+              className="justify-items-center w-full text-6xl "
+            >
+              <NotesIcon
+                fontSize="inherit"
+                className="ml-auto mr-auto mt-auto mb-auto "
+              ></NotesIcon>
             </CardBody>
-            <div className="text-5xl text-center mt-auto row-start-2">+</div>
+            <CardFooter className="text-sm">Template 1</CardFooter>
           </Card>
+        </div>
+        <div className="w-full bg-gray-800 row-span-2">
           {isLoading != true
             ? data?.map((board: Board) => (
                 <BoardCard key={board.id} board={board}></BoardCard>
               ))
             : null}
         </div>
-        <div className="w-full bg-gray-800 row-span-2"></div>
       </div>
     </>
   );
