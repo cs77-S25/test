53330d81beea628b89f34fbdd5ad5c40f241efcb
jaron1234 <jarontinsley12@gmail.com>
2025-04-16 23:42:39 -0400

    Button Update II

--- src/components/tiptap.tsx
+++ src/components/tiptap.tsx
@@ -12,7 +12,7 @@ import {
 } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import React, { useEffect } from "react";
-import { Button, Card, CardBody, CardHeader } from "@heroui/react";
+import { Button, Card, CardBody, CardHeader, ButtonGroup } from "@heroui/react";
 import { updateDoc } from "@/app/actions/actions";
 import { useDebouncedCallback } from "use-debounce";
 
@@ -81,126 +81,185 @@ export default (props: any) => {
       <CardHeader>
         <div className="control-group">
           <div className="button-group">
-            <Button
-              onPress={() => editor.chain().focus().toggleItalic().run()}
-              disabled={!editor.can().chain().focus().toggleItalic().run()}
-              className={editor.isActive("italic") ? "is-active" : ""}
-            >
-              <FormatItalicIcon></FormatItalicIcon>
-            </Button>
-            <Button
-              onPress={() => editor.chain().focus().toggleBold().run()}
-              disabled={!editor.can().chain().focus().toggleBold().run()}
-              className={editor.isActive("bold") ? "is-active" : ""}
-            >
-              <FormatBoldIcon></FormatBoldIcon>
-            </Button>
-            <Button
-              onPress={() => editor.chain().focus().toggleStrike().run()}
-              disabled={!editor.can().chain().focus().toggleStrike().run()}
-              className={editor.isActive("strike") ? "is-active" : ""}
-            >
-              <StrikethroughSIcon></StrikethroughSIcon>
-            </Button>
-            <Button
-              onPress={() => editor.chain().focus().toggleCode().run()}
-              disabled={!editor.can().chain().focus().toggleCode().run()}
-              className={editor.isActive("code") ? "is-active" : ""}
-            >
-              <CodeIcon></CodeIcon>
-            </Button>
-            <Button
-              onPress={() => editor.chain().focus().unsetAllMarks().run()}
-            >
-              <ClearAllIcon></ClearAllIcon>
-            </Button>
-            <Button onPress={() => editor.chain().focus().clearNodes().run()}>
-              <FormatClearIcon></FormatClearIcon>
-            </Button>
-            <Button
-              onPress={() => editor.chain().focus().setParagraph().run()}
-              className={editor.isActive("paragraph") ? "is-active" : ""}
-            >
-              <FormatTextdirectionLToRIcon></FormatTextdirectionLToRIcon>
-            </Button>
-            <Button
-              onPress={() =>
-                editor.chain().focus().toggleHeading({ level: 1 }).run()
-              }
-              className={
-                editor.isActive("heading", { level: 1 }) ? "is-active" : ""
-              }
-            >
-              Header
-            </Button>
-
-          <Button
-            //onClick={() => editor.chain().focus().setFontFamily('serif').run()}
-            className={editor.isActive('textStyle', { fontFamily: 'serif' }) ? 'is-active' : ''}
-            data-test-id="serif"
-          >
-            Serif
-          </Button>
-
-            <Button
-              onPress={() =>
-                editor.chain().focus().toggleHeading({ level: 2 }).run()
-              }
-              className={
-                editor.isActive("heading", { level: 2 }) ? "is-active" : ""
-              }
-            >
-              H2
-            </Button>
-            
-            
-            <Button
-              onPress={() => editor.chain().focus().toggleBulletList().run()}
-              className={editor.isActive("bulletList") ? "is-active" : ""}
-            >
-              <FormatListBulletedIcon></FormatListBulletedIcon>
-            </Button>
-            <Button
-              onPress={() => editor.chain().focus().toggleOrderedList().run()}
-              className={editor.isActive("orderedList") ? "is-active" : ""}
-            >
-              <FormatListNumberedIcon></FormatListNumberedIcon>
-            </Button>
-            <Button
-              onPress={() => editor.chain().focus().toggleBlockquote().run()}
-              className={editor.isActive("blockquote") ? "is-active" : ""}
-            >
-              <FormatQuoteIcon></FormatQuoteIcon>
-            </Button>
-            <Button
-              onPress={() => editor.chain().focus().undo().run()}
-              disabled={!editor.can().chain().focus().undo().run()}
-            >
-              <UndoIcon></UndoIcon>
-            </Button>
-            <Button
+            <ButtonGroup>
+              <Button
+                color = "secondary"
+                variant = "ghost"
+                onPress={() => editor.chain().focus().toggleItalic().run()}
+                disabled={!editor.can().chain().focus().toggleItalic().run()}
+                className={editor.isActive("italic") ? "is-active" : ""}
+              >
+                <FormatItalicIcon></FormatItalicIcon>
+              </Button>
+              <Button
+                color = "secondary"
+                variant = "ghost"
+                onPress={() => editor.chain().focus().toggleBold().run()}
+                disabled={!editor.can().chain().focus().toggleBold().run()}
+                className={editor.isActive("bold") ? "is-active" : ""}
+              >
+                <FormatBoldIcon></FormatBoldIcon>
+              </Button>
+              <Button
+                color = "secondary"
+                variant = "ghost"
+                onPress={() => editor.chain().focus().toggleStrike().run()}
+                disabled={!editor.can().chain().focus().toggleStrike().run()}
+                className={editor.isActive("strike") ? "is-active" : ""}
+              >
+                <StrikethroughSIcon></StrikethroughSIcon>
+              </Button>
+              <Button
+                color = "secondary"
+                variant = "ghost"
+                onPress={() => editor.chain().focus().toggleCode().run()}
+                disabled={!editor.can().chain().focus().toggleCode().run()}
+                className={editor.isActive("code") ? "is-active" : ""}
+              >
+                <CodeIcon></CodeIcon>
+              </Button>
+              <Button
+                color = "secondary"
+                variant = "ghost"
+                onPress={() => editor.chain().focus().unsetAllMarks().run()}
+              >
+                <ClearAllIcon></ClearAllIcon>
+              </Button>
+              <Button
+                color = "secondary"
+                variant = "ghost" 
+                onPress={() => editor.chain().focus().clearNodes().run()}>
+                <FormatClearIcon></FormatClearIcon>
+              </Button>
+              <Button
+                color = "secondary"
+                variant = "ghost"
+                onPress={() => editor.chain().focus().setParagraph().run()}
+                className={editor.isActive("paragraph") ? "is-active" : ""}
+              >
+                <FormatTextdirectionLToRIcon></FormatTextdirectionLToRIcon>
+              </Button>
               
-              color = "warning"
-              onPress={() => editor.chain().focus().redo().run()}
-              disabled={!editor.can().chain().focus().redo().run()}
-            >
-              <RedoIcon></RedoIcon>
+              <Button
+                color = "secondary"
+                variant = "ghost"
+                onPress={() => editor.chain().focus().toggleBulletList().run()}
+                className={editor.isActive("bulletList") ? "is-active" : ""}
+              >
+                <FormatListBulletedIcon></FormatListBulletedIcon>
+              </Button>
+              <Button
+                color = "secondary"
+                variant = "ghost"
+                onPress={() => editor.chain().focus().toggleOrderedList().run()}
+                className={editor.isActive("orderedList") ? "is-active" : ""}
+              >
+                <FormatListNumberedIcon></FormatListNumberedIcon>
+              </Button>
+              <Button
+                color = "secondary"
+                variant = "ghost"
+                onPress={() => editor.chain().focus().toggleBlockquote().run()}
+                className={editor.isActive("blockquote") ? "is-active" : ""}
+              >
+                <FormatQuoteIcon></FormatQuoteIcon>
+              </Button>
+              <Button
+                color = "secondary"
+                variant = "ghost"
+                onPress={() => editor.chain().focus().undo().run()}
+                disabled={!editor.can().chain().focus().undo().run()}
+              >
+                <UndoIcon></UndoIcon>
+              </Button>
+              <Button
+                color = "secondary"
+                variant = "ghost"
+                onPress={() => editor.chain().focus().redo().run()}
+                disabled={!editor.can().chain().focus().redo().run()}
+              >
+                <RedoIcon></RedoIcon>
+              </Button>
+              {/* <Button
+                onPress={() => editor.chain().focus().setColor("#958DF1").run()}
+                className={
+                  editor.isActive("textStyle", { color: "#958DF1" })
+                    ? "is-active"
+                    : ""
+                }
+              >
+                Purple
+              </Button> */}
+            </ButtonGroup>
+            <ButtonGroup>
+            <Button
+                color = "secondary"
+                variant = "bordered"
+              >
+                Size: 
             </Button>
-            {/* <Button
-              onPress={() => editor.chain().focus().setColor("#958DF1").run()}
-              className={
-                editor.isActive("textStyle", { color: "#958DF1" })
-                  ? "is-active"
-                  : ""
-              }
-            >
-              Purple
-            </Button> */}
+            <Button
+                color = "secondary"
+                variant = "ghost"
+                onPress={() =>
+                  editor.chain().focus().toggleHeading({ level: 4 }).run()
+                }
+                className={
+                  editor.isActive("heading", { level: 4 }) ? "is-active" : ""
+                }
+              >
+                1
+            </Button>
+            <Button
+                color = "secondary"
+                variant = "ghost"
+                onPress={() =>
+                  editor.chain().focus().toggleHeading({ level: 3 }).run()
+                }
+                className={
+                  editor.isActive("heading", { level: 3 }) ? "is-active" : ""
+                }
+              >
+                2
+            </Button>
+            <Button
+                color = "secondary"
+                variant = "ghost"
+                onPress={() =>
+                  editor.chain().focus().toggleHeading({ level: 2 }).run()
+                }
+                className={
+                  editor.isActive("heading", { level: 2 }) ? "is-active" : ""
+                }
+              >
+                3
+            </Button>
+            <Button
+                color = "secondary"
+                variant = "ghost"
+                onPress={() =>
+                  editor.chain().focus().toggleHeading({ level: 1 }).run()
+                }
+                className={
+                  editor.isActive("heading", { level: 1 }) ? "is-active" : ""
+                }
+              >
+                4
+            </Button>
+            </ButtonGroup>
           </div>
         </div>
       </CardHeader>
     );
   };
+
+  //Graveryard
+  /*<Button
+      //onClick={() => editor.chain().focus().setFontFamily('serif').run()}
+      className={editor.isActive('textStyle', { fontFamily: 'serif' }) ? 'is-active' : ''}
+      data-test-id="serif">
+      Serif
+    </Button> */
 
   useEffect(() => {
     console.log(editorContent);
